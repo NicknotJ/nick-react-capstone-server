3612ea7a578c56d37259d31a50470e5bf27ce1e3
nicknotj <nicknotjustin@gmail.com>
2019-04-16 15:34:23 -0500

    If it was just the / ruining the CORS thing, I am going to start drinking.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(morgan('common'));
 
 // CORS
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://jovial-allen-c95d7e.netlify.com/');
+  res.header('Access-Control-Allow-Origin', 'https://jovial-allen-c95d7e.netlify.com');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   if (req.method === 'OPTIONS') {
