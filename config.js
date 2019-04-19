88fdbbc996bf330d6351659582ba318c1c1b8254
nicknotj <nicknotjustin@gmail.com>
2019-04-19 09:37:16 -0500

    changed CORS to SITES and config to check for an environmental variable.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/pain-journal';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://codern:password123@ds137255.mlab.com:37255/react-capstone-pain-journal' //'mongodb://localhost/pain-journal';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/pain-journal';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
